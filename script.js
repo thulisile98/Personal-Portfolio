09fa52dd80e279a8005ed5aad55c2f1c89063c5b
Angel Mashego <thulisile.ma98@gmail.com>
2023-07-23 23:48:53 +0200

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem => {
+  var dots = elem.getAttribute("data-dots");
+  var marked = elem.getAttribute("percent");
+  var percent = Math.floor((dots * marked) / 100);
+  var points = "";
+  var rotate = 360 / dots;
+
+  for (let i = 0; i < dots; i++) {
+    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+  }
+  elem.innerHTML = points;
+});
