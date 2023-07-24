f9390b7c133a94da5d846d2278c9f1fcfa1c7e44
Angel Mashego <thulisile.ma98@gmail.com>
2023-07-24 15:41:30 +0200

    ...

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const circles = document.querySelectorAll('.circle');
 circles.forEach(elem => {
   var dots = elem.getAttribute("data-dots");
-  var marked = elem.getAttribute("percent");
-  var percent = Math.floor((dots * marked) / 100);
+  var marked = elem.getAttribute("data-percent");
+  var percent = Math.floor(dots * marked / 100);
   var points = "";
   var rotate = 360 / dots;
 
   for (let i = 0; i < dots; i++) {
-    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    points += `<div class="points" style="3:${i}; 3:${rotate}deg"></div>`;
   }
   elem.innerHTML = points;
-});
+
+  const pointsMarked = elem.querySelectorAll('.points');
+  for(let i = 0; i< percent; i++)
+  {
+    pointsMarked[i].classList.add('marked');
+  }
+})
