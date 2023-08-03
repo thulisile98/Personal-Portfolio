7d5f247ba6f9fc6150b96dd430eedabdde779ce6
Angel Mashego <thulisile.ma98@gmail.com>
2023-08-03 15:59:11 +0200

    ..

---
+++ script.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
+
+var typingEffect = new Typed(".multiText",{
+    strings:["DEVELOPER","DESIGNER"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:80,
+    backDelay:1500
+})
